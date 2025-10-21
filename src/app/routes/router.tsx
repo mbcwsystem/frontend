647bc1a392b09ae0c42d79d11f0b574e563d78e2
G-jjunny <rudwns9551@naver.com>
2025-10-21 18:31:51 +0900

    init: 초기 환경 설정

---
+++ src/app/routes/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+import { ROUTES } from "./routes";
+
+export const router = createBrowserRouter([
+  {
+    path: ROUTES.ROOT,
+  },
+]);
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,6 @@
+/** 해당 파일에서 라우팅 경로를 관리합니다 */
+
+export const ROUTES = {
+  ROOT: "/",
+  AUTH: "/auth",
+} as const;
