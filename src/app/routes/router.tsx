5f612b8f73b0ec82e4136d1d850636e5609a2ea4
G-jjunny <rudwns9551@naver.com>
2025-10-21 18:43:18 +0900

    init: app레이어 슬라이스 분리 및 레이아웃 세그먼트 추가

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import { ROUTES } from "./routes";
+import { Layout } from "../global/Layout";
 
 export const router = createBrowserRouter([
   {
     path: ROUTES.ROOT,
+    element: <Layout />,
   },
 ]);
