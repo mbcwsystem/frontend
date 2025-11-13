5c7449fb0a65b5a0638c80015c9c59ee000f14ce
G-jjunny <rudwns9551@naver.com>
2025-11-13 16:33:26 +0900

    init: github webkook test

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -2,11 +2,18 @@ import { createBrowserRouter } from 'react-router';
 
 import { ROUTES } from './routes';
 
+import HomePage from '@/pages/HomePage';
 import { Layout } from '@/shared/layouts/Layout';
 
 export const router = createBrowserRouter([
   {
     path: ROUTES.ROOT,
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
