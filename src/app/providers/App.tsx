20704e9ba9629a92333f80bc2a6905c937cc5072
G-jjunny <rudwns9551@naver.com>
2025-11-13 16:23:13 +0900

    init: eslint, prettier 규칙 설정 및 적용

--- src/app/providers/App.tsx
+++ src/app/providers/App.tsx
@@ -1,5 +1,6 @@
-import { RouterProvider } from "react-router";
-import { router } from "../routes/router";
+import { RouterProvider } from 'react-router';
+
+import { router } from '../routes/router';
 
 function App() {
   return <RouterProvider router={router} />;
