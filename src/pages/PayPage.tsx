7c43e997f93210a2ba6f845ef93606e9608e16db
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-11-19 15:07:35 +0900

    chore: husky pre-push edit

--- src/pages/PayPage.tsx
+++ src/pages/PayPage.tsx
@@ -1,13 +1,13 @@
 export default function PayPage() {
   return (
     <>
-        <div className="flex flex-col">
-            <div className=" bg-amber-500 h-50">gg</div>
-            <div className="flex flex-row  ">
-                <div>dd</div>
-                <div>dd</div>
-            </div>
+      <div className="flex flex-col">
+        <div className=" bg-amber-500 h-50">gg</div>
+        <div className="flex flex-row  ">
+          <div>dd</div>
+          <div>dd</div>
         </div>
+      </div>
     </>
-  )
-};
+  );
+}
