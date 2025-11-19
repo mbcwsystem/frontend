420efeefefa78b1c4b35f07ac49858edd5bdfe49
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-11-19 14:20:37 +0900

    chore: tailwind 설정 수정

---
+++ src/pages/PayPage.tsx
@@ -0,0 +1,13 @@
+export default function PayPage() {
+  return (
+    <>
+        <div className="flex flex-col">
+            <div className=" ">gg</div>
+            <div className="flex flex-row  ">
+                <div>dd</div>
+                <div>dd</div>
+            </div>
+        </div>
+    </>
+  )
+};
