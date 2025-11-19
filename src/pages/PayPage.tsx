4f6ecc7aa68ee3ad505acf6ac5762ac2fa058f53
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-11-19 14:53:52 +0900

    chore: tailwind 재테스트 및 수정

--- src/pages/PayPage.tsx
+++ src/pages/PayPage.tsx
@@ -2,7 +2,7 @@ export default function PayPage() {
   return (
     <>
         <div className="flex flex-col">
-            <div className=" ">gg</div>
+            <div className=" bg-amber-500 h-50">gg</div>
             <div className="flex flex-row  ">
                 <div>dd</div>
                 <div>dd</div>
