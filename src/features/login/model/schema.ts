78477a2de0cd4821e77c2ba4bcbfc8fc1a35e304
G-jjunny <rudwns9551@naver.com>
2025-11-23 16:21:17 +0900

    refactor: login schema수정

--- src/features/login/model/schema.ts
+++ src/features/login/model/schema.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 const loginSchema = z.object({
   userId: z.string().min(4, {
-    message: 'UserID must be at least 2 characters.',
+    message: '아이디를 입력해주세요',
   }), // userID에 대한 스키마 정의
   password: z.string().min(6, {
-    message: 'Password must be at least 8 characters.',
+    message: '비밀번호를 입력해주세요',
   }), // userPassword에 대한 스키마 정의
 });
 
