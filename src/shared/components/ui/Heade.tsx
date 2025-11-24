b3377c8396558d4fad2ed54d29c78ea64bdb9717
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-11-24 17:39:27 +0900

    design: header UI 틀 구성 및 호버 적용

--- src/shared/components/ui/Heade.tsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return(
-        <>
-            <div className="h-15 bg-gray-50">
-
-            </div>
-        </>
-    )
-};
---
+++ src/shared/components/ui/Header.tsx
@@ -0,0 +1,39 @@
+import { useMemo } from 'react';
+import '../../../app/global/App.css';
+import logo from '../../../shared/components/logo/Megabox_Logo_Indigo.png';
+import { Bell } from 'lucide-react';
+import { KeyRound } from 'lucide-react';
+import { Lock } from 'lucide-react';
+import { CirclePlus } from 'lucide-react';
+
+export const Header = () => {
+
+  const today = useMemo(() => {
+    return new Date().toISOString().slice(0, 10);
+  }, []);
+
+  const iconHover =
+    'transition-all duration-200 hover:scale-110 hover:text-gray-500 cursor-pointer';
+  const logoHover = 'transition-all duration-200 cursor-pointer';
+
+  return (
+    <>
+      <div className="h-16 bg-[#EAF3F4] flex justify-between items-center px-5">
+        {/* 왼쪽 로고 + 타이틀 */}
+        <div className="flex items-center gap-4">
+          <img src={logo} alt="logo" className={`${logoHover} h-7`} />
+          <div className="font-bold">M SYSTEM</div>
+        </div>
+        {/* 오른쪽 유저 이름 (메박이) + 날짜 */}
+        <div className="flex items-center gap-4">
+          <div className="text-[#5FB3D3] text-sm"> 메박이 </div>
+          <div className="font-light"> {today} </div>
+          <Bell size={20} strokeWidth={3} className={iconHover} />
+          <KeyRound size={20} strokeWidth={3} className={iconHover} />
+          <Lock size={20} strokeWidth={3} className={iconHover} />
+          <CirclePlus size={20} strokeWidth={3} className={iconHover} />
+        </div>
+      </div>
+    </>
+  );
+};
