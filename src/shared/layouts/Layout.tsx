630e47f7293704283053f5d49949ea9529d5cf74
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-11-25 16:57:18 +0900

    refactor: Header 파일 위치 변경

--- src/shared/layouts/Layout.tsx
+++ src/shared/layouts/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router';
 
-import { Header } from '../components/ui/Header';
+import { Header } from '../../widgets/Header';
 
 export const Layout = () => {
   return (
