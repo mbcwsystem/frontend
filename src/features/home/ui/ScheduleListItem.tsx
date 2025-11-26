88482d87a32ca457a0d405323617c2244730b30d
G-jjunny <rudwns9551@naver.com>
2025-11-26 23:16:06 +0900

    deesign: ScheduleListItem 스타일 변경

--- src/features/home/ui/ScheduleListItem.tsx
+++ src/features/home/ui/ScheduleListItem.tsx
@@ -1,9 +1,9 @@
 const ScheduleListItem = () => {
   return (
-    <div className=" bg-mega/20 gap-3 flex flex-col px-3 py-2 rounded-md">
+    <div className=" bg-muted-foreground/10 gap-2 flex px-3 py-2 rounded-md items-center before:w-1 before:self-stretch before:bg-muted-foreground/10 before:rounded-md">
       <div className=" flex flex-col text-xs">
         <p className=" font-bold">야간 근무</p>
-        <p>May 9, 9:00 - 10:00</p>
+        <p className=" text-muted-foreground">May 9, 9:00 - 10:00</p>
       </div>
     </div>
   );
