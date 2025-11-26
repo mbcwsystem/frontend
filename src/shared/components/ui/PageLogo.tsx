2b535e77c00fd60deb190e6242d8e56e189fb680
G-jjunny <rudwns9551@naver.com>
2025-11-26 22:39:12 +0900

    design: pageLogo 크기 수정

--- src/shared/components/ui/PageLogo.tsx
+++ src/shared/components/ui/PageLogo.tsx
@@ -3,8 +3,8 @@ import Logo from '../../assets/logo/Logo_white.png';
 const PageLogo = () => {
   return (
     <div className=" w-full flex flex-col gap-4 justify-center items-center ">
-      <img src={Logo} alt="mega-logo" className=" w-[200px] md:w-[250px]" />
-      <h2 className=" text-2xl text-white">M SYSTEM에 오신 것을 환영합니다.</h2>
+      <img src={Logo} alt="mega-logo" className=" w-[120px] md:w-[180px]" />
+      <h2 className=" md:text-xl md:text-md text-white">M SYSTEM에 오신 것을 환영합니다.</h2>
     </div>
   );
 };
