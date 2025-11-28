cc75cbd9f0a2d14e6e868cb9abe7034e7df886e9
G-jjunny <rudwns9551@naver.com>
2025-11-28 13:18:55 +0900

    feat: avarar컴포넌트 추가

--- src/features/home/ui/PayHeader.tsx
+++ src/features/home/ui/PayHeader.tsx
@@ -1,54 +1,65 @@
-// import { Item, ItemMedia } from '@/shared/components/ui/item';
+import { Plus } from 'lucide-react';
 
-// const PayHeader = () => {
-//   return (
-//     <div className="flex w-full max-w-lg flex-col gap-6">
-//       <Item variant="outline">
-//         <ItemMedia>
-//           <Avatar className="size-10">
-//             <AvatarImage src="https://github.com/evilrabbit.png" />
-//             <AvatarFallback>ER</AvatarFallback>
-//           </Avatar>
-//         </ItemMedia>
-//         <ItemContent>
-//           <ItemTitle>Evil Rabbit</ItemTitle>
-//           <ItemDescription>Last seen 5 months ago</ItemDescription>
-//         </ItemContent>
-//         <ItemActions>
-//           <Button size="icon-sm" variant="outline" className="rounded-full" aria-label="Invite">
-//             <Plus />
-//           </Button>
-//         </ItemActions>
-//       </Item>
-//       <Item variant="outline">
-//         <ItemMedia>
-//           <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-//             <Avatar className="hidden sm:flex">
-//               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-//               <AvatarFallback>CN</AvatarFallback>
-//             </Avatar>
-//             <Avatar className="hidden sm:flex">
-//               <AvatarImage src="https://github.com/maxleiter.png" alt="@maxleiter" />
-//               <AvatarFallback>LR</AvatarFallback>
-//             </Avatar>
-//             <Avatar>
-//               <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
-//               <AvatarFallback>ER</AvatarFallback>
-//             </Avatar>
-//           </div>
-//         </ItemMedia>
-//         <ItemContent>
-//           <ItemTitle>No Team Members</ItemTitle>
-//           <ItemDescription>Invite your team to collaborate on this project.</ItemDescription>
-//         </ItemContent>
-//         <ItemActions>
-//           <Button size="sm" variant="outline">
-//             Invite
-//           </Button>
-//         </ItemActions>
-//       </Item>
-//     </div>
-//   );
-// };
+import { Avatar, AvatarFallback, AvatarImage } from '@/shared/components/ui/avatar';
+import { Button } from '@/shared/components/ui/button';
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from '@/shared/components/ui/item';
 
-// export default PayHeader;
+const PayHeader = () => {
+  return (
+    <div className="flex w-full max-w-lg flex-col gap-6">
+      <Item variant="outline">
+        <ItemMedia>
+          <Avatar className="size-10">
+            <AvatarImage src="https://github.com/evilrabbit.png" />
+            <AvatarFallback>ER</AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Evil Rabbit</ItemTitle>
+          <ItemDescription>Last seen 5 months ago</ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button size="icon-sm" variant="outline" className="rounded-full" aria-label="Invite">
+            <Plus />
+          </Button>
+        </ItemActions>
+      </Item>
+      <Item variant="outline">
+        <ItemMedia>
+          <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
+            <Avatar className="hidden sm:flex">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <Avatar className="hidden sm:flex">
+              <AvatarImage src="https://github.com/maxleiter.png" alt="@maxleiter" />
+              <AvatarFallback>LR</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
+              <AvatarFallback>ER</AvatarFallback>
+            </Avatar>
+          </div>
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>No Team Members</ItemTitle>
+          <ItemDescription>Invite your team to collaborate on this project.</ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button size="sm" variant="outline">
+            Invite
+          </Button>
+        </ItemActions>
+      </Item>
+    </div>
+  );
+};
+
+export default PayHeader;
