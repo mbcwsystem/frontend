642ae83201fb2337db290299102c910f65b1eab9
G-jjunny <rudwns9551@naver.com>
2025-12-06 18:29:05 +0900

    feat: workstatus form 생성

--- src/features/work-status/ui/WorkStatus.tsx
+++ src/features/work-status/ui/WorkStatus.tsx
@@ -1,5 +1,63 @@
+import { Button } from '@/shared/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/shared/components/ui/card';
+import { Input } from '@/shared/components/ui/input';
+import { Label } from '@/shared/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/components/ui/tabs';
+
 const WorkStatus = () => {
-  return <div>WorkStats</div>;
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="attendance">출퇴근</TabsTrigger>
+          <TabsTrigger value="rest">휴식 및 복귀</TabsTrigger>
+        </TabsList>
+        <TabsContent value="attendance">
+          <Card>
+            <CardHeader>
+              <CardTitle>관리자님 환영합니다</CardTitle>
+              {/* <CardDescription>
+                Make changes to your account here. Click save when you&apos;re done.
+              </CardDescription> */}
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-name">Name</Label>
+                <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-username">Username</Label>
+                <Input id="tabs-demo-username" defaultValue="@peduarte" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save changes</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="rest">
+          <Card>
+            <CardHeader>
+              <CardTitle>관리자님 환영합니다</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-current">Current password</Label>
+                <Input id="tabs-demo-current" type="password" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-new">New password</Label>
+                <Input id="tabs-demo-new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default WorkStatus;
