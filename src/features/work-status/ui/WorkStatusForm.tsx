4894e282ae5994d58df300f656654de29309cb9e
G-jjunny <rudwns9551@naver.com>
2025-12-08 16:37:19 +0900

    test: husky test

--- src/features/work-status/ui/WorkStatusForm.tsx
+++ src/features/work-status/ui/WorkStatusForm.tsx
@@ -7,7 +7,7 @@ import { STATUS_TYPES } from '../model/status';
 import { Button } from '@/shared/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/components/ui/card';
 import { Form } from '@/shared/components/ui/form';
-import RHFInput from '@/shared/components/ui/RHFinput';
+import RHFInput from '@/shared/components/ui/RHFInput';
 
 interface WorkStatusFormProps {
   type: (typeof STATUS_TYPES)[keyof typeof STATUS_TYPES];
@@ -36,7 +36,7 @@ const WorkStatusForm = ({ type }: WorkStatusFormProps) => {
 
       <CardContent className="flex flex-col gap-4">
         <Form {...form}>
-          <form id="work-status-form">
+          <form id="work-status-form" className=" flex flex-col gap-2">
             <RHFInput form={form} name="username" placeholder="ID" />
             <RHFInput form={form} name="password" placeholder="PASSWORD" />
           </form>
