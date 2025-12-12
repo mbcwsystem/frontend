32d336dc084793b30b0630ba162c7d6d403efa46
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-12-12 17:10:28 +0900

    design: commuPage 사이드바 및 메인 카드 구현

--- src/pages/community/ui/Community.tsx
+++ src/pages/community/ui/Community.tsx
@@ -1,10 +1,27 @@
-const Communiity = () => {
+import { Card, CardContent } from "@/shared/components/ui/card";
+import { TextAlignStart } from "lucide-react";
+
+export default function Communiity() {
 
     return(
-        <>
-        <div> 커뮤니티 페이지 입니다. </div>
-        </>
+    <div className="flex flex-col gap-5 w-full">
+        <div className="text-2xl font-bold">커뮤니티</div>
+        <div className="flex gap-10">
+        <Card variant="blueSide" >
+            <CardContent>
+                <div className="flex gap-3 items-center">
+                    <TextAlignStart size={16} strokeWidth={3}/>
+                    <div>목록</div>
+                </div>
+                
+            </CardContent>
+        </Card>
+        <Card variant="blueMain" >
+            <CardContent>
+
+            </CardContent>
+        </Card>
+        </div>
+    </div>
     )
 }
-
-export default Communiity;
