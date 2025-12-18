d055255036517db8b3b8d4dd4595271bffc92881
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-12-18 14:31:27 +0900

    add: freeboard 목업파일 추가

--- src/pages/community/index.ts
+++ src/pages/community/index.ts
@@ -3,4 +3,4 @@ export { default as NoticePage } from './ui/notice/NoticePage';
 export { default as NoticeDetail } from './ui/notice/NoticeDetail';
 export { default as ShiftPage } from './ui/ShiftPage';
 export { default as DayoffPage } from './ui/DayoffPage';
-export { default as FreeboardPage } from './ui/FreeboardPage';
+export { default as FreeboardPage } from './ui/freeboard/FreeboardPage';
