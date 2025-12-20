26b7e6479eebbd0488928f7cbd235be4b698bff4
박하늘 <parkhaneul@baghaneul-ui-MacBookAir.local>
2025-12-20 21:40:44 +0900

    fix: 미사용 import 제거

--- src/features/community/ui/comment/CommentList.tsx
+++ src/features/community/ui/comment/CommentList.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import CommentItem from './CommentItem';
 import Pagenation from '../../ui/Pagenation';
 
--- src/features/community/ui/dayoff/DayoffBoard.tsx
+++ src/features/community/ui/dayoff/DayoffBoard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Link } from 'react-router';
 import type { DayoffPost } from '@/features/community/mock/communityMock';
 import { APPROVAL_STATUS_LABEL, APPROVAL_STATUS_STYLE } from '../../model/statusLabel';
