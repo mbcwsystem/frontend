272e3b5148a98ed7db8cfcb4c42bdaa71273687b
G-jjunny <rudwns9551@naver.com>
2026-02-18 18:25:25 +0900

    feat: 로그인 api로직 책임 분리

--- src/features/login/api/queries.ts
+++ src/features/login/api/queries.ts
@@ -1,10 +1,36 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router';
+import { toast } from 'sonner';
+
 import { authService } from './service';
 
-export const authQueries = {
-  login: {
+import { queryClient } from '@/shared/api/queryClient';
+import { ROUTES } from '@/shared/constants/routes';
+import { useAuthStore } from '@/shared/model/authStore';
+
+// export const authQueries = {
+//   login: {
+//     mutationFn: authService.login,
+//   },
+//   logout: {
+//     mutationFn: authService.logout,
+//   },
+// };
+
+export const useLoginMutation = () => {
+  const { setAccessToken, setRefreshToken, setAuth } = useAuthStore();
+  const navigate = useNavigate();
+  return useMutation({
     mutationFn: authService.login,
-  },
-  logout: {
-    mutationFn: authService.logout,
-  },
+    onSuccess: (data) => {
+      setAccessToken(data.access_token);
+      setRefreshToken(data.refresh_token);
+      setAuth();
+
+      void queryClient.invalidateQueries({ queryKey: ['me'] });
+
+      toast.success('로그인 성공!');
+      void navigate(ROUTES.ROOT);
+    },
+  });
 };
--- src/features/login/api/service.ts
+++ src/features/login/api/service.ts
@@ -1,35 +1,40 @@
 import type { LoginRequestDTO, LoginResponseDTO, LogOutResponseDTO } from './dto';
 
 import { apiClient } from '@/shared/api/apiClients';
+import { queryClient } from '@/shared/api/queryClient';
 import { useAuthStore } from '@/shared/model/authStore';
 
 export const authService = {
-  login: async (data: LoginRequestDTO): Promise<LoginResponseDTO> => {
-    const response = await apiClient.post<LoginResponseDTO>({
-      url: '/api/auth/login',
-      data,
-    });
-    const token = response.access_token;
-    const refreshToken = response.refresh_token;
+  // login: async (data: LoginRequestDTO): Promise<LoginResponseDTO> => {
+  //   const response = await apiClient.post<LoginResponseDTO>({
+  //     url: '/api/auth/login',
+  //     data,
+  //   });
+  //   const token = response.access_token;
+  //   const refreshToken = response.refresh_token;
 
-    if (!token) {
-      throw new Error('로그인 실패: 토큰을 받지 못했습니다.');
-    }
+  //   if (!token) {
+  //     throw new Error('로그인 실패: 토큰을 받지 못했습니다.');
+  //   }
 
-    // Zustand store 업데이트
-    const { setAccessToken, setRefreshToken, setAuth } = useAuthStore.getState();
-    setAccessToken(token);
-    setRefreshToken(refreshToken);
-    setAuth();
+  //   // Zustand store 업데이트
+  //   const { setAccessToken, setRefreshToken, setAuth } = useAuthStore.getState();
+  //   setAccessToken(token);
+  //   setRefreshToken(refreshToken);
+  //   setAuth();
 
-    return response;
-  },
+  //   return response;
+  // },
+  login: (data: LoginRequestDTO) =>
+    apiClient.post<LoginResponseDTO>({ url: '/api/auth/login', data }),
+
   logout: async (): Promise<LogOutResponseDTO> => {
     const response = await apiClient.post<LogOutResponseDTO>({
       url: '/api/auth/logout',
     });
     const { clearAuth } = useAuthStore.getState();
     clearAuth();
+    queryClient.clear();
 
     return response;
   },
